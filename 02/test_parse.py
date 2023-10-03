dca5c1e33823f028115f3b224f0a7831f9ebf634
Dadypool <dudypool@yandex.ru>
2023-10-03 05:02:01 +0300

    Init test for parser

---
+++ 02/test_parse.py
@@ -0,0 +1,12 @@
+"Homework 2 with tests for parser func"
+
+import unittest
+from unittest.mock import patch
+
+from parse import parse_json
+
+
+class TestParse(unittest.TestCase):
+    "Class with tests"
+
+    
