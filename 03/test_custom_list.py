20d024587ef0ab77db71a92b4e2282e5b3eaa309
Dadypool <dudypool@yandex.ru>
2023-10-09 20:24:34 +0300

    Fixed some tests

--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -8,103 +8,236 @@ from custom_list import CustomList
 class TestDecor(TestCase):
     "Class with tests"
 
-    def setUp(self):
-        self.cust_list1 = CustomList([5, 1, 3, 7])
-
     def test_base_work(self):
         "Test indexing and append, extend, pop, insert, del, remove, len funcs"
 
-        self.cust_list1.pop(1)
-        self.assertEqual(self.cust_list1, CustomList([5, 3, 7]))
-        self.assertEqual(3, len(self.cust_list1))
+        cust_list1 = CustomList([5, 1, 3, 7])
+        cust_list1.pop(1)
+        expect_list = CustomList([5, 3, 7])
+        self.assertEqual(len(expect_list), len(cust_list1))
+        for i, j in zip(cust_list1, expect_list):
+            self.assertEqual(i, j)
 
-        self.cust_list1.append(0)
-        self.assertEqual(self.cust_list1, CustomList([5, 3, 7, 0]))
+        cust_list1.append(0)
+        expect_list = CustomList([5, 3, 7, 0])
+        self.assertEqual(len(expect_list), len(cust_list1))
+        for i, j in zip(cust_list1, expect_list):
+            self.assertEqual(i, j)
 
-        del self.cust_list1[2]
-        self.assertEqual(self.cust_list1, CustomList([5, 3, 0]))
+        del cust_list1[2]
+        expect_list = CustomList([5, 3, 0])
+        self.assertEqual(len(expect_list), len(cust_list1))
+        for i, j in zip(cust_list1, expect_list):
+            self.assertEqual(i, j)
 
-        self.cust_list1.insert(2, 10)
-        self.assertEqual(self.cust_list1, CustomList([5, 3, 10, 0]))
+        cust_list1.insert(2, 10)
+        expect_list = CustomList([5, 3, 10, 0])
+        self.assertEqual(len(expect_list), len(cust_list1))
+        for i, j in zip(cust_list1, expect_list):
+            self.assertEqual(i, j)
 
-        self.cust_list1.remove(3)
-        self.assertEqual(self.cust_list1, CustomList([5, 10, 0]))
+        cust_list1.remove(3)
+        expect_list = CustomList([5, 10, 0])
+        self.assertEqual(len(expect_list), len(cust_list1))
+        for i, j in zip(cust_list1, expect_list):
+            self.assertEqual(i, j)
 
-        self.cust_list1.extend([3, 4])
-        self.assertEqual(self.cust_list1, CustomList([5, 10, 0, 3, 4]))
+        cust_list1.extend([3, 4])
+        expect_list = CustomList([5, 10, 0, 3, 4])
+        self.assertEqual(len(expect_list), len(cust_list1))
+        for i, j in zip(cust_list1, expect_list):
+            self.assertEqual(i, j)
 
-    def test_add(self):
-        "Test add"
+    def test_add_same_size(self):
+        "Test add same size CustomLists"
 
-        # Same size
+        cust_list1 = CustomList([5, 1, 3, 7])
         cust_list2 = CustomList([1, 1, 1, 1])
-        self.assertEqual(self.cust_list1 + cust_list2, CustomList([6, 2, 4, 8]))
+        res_list = cust_list1 + cust_list2
+        exp_list = CustomList([6, 2, 4, 8])
+        # Check resulted list
+        self.assertEqual(len(res_list), len(exp_list))
+        for i, j in zip(res_list, exp_list):
+            self.assertEqual(i, j)
+        # Check cust_list1 and cust_list2 didnt changed
+        for i, j in zip(cust_list1, CustomList([5, 1, 3, 7])):
+            self.assertEqual(i, j)
+        for i, j in zip(cust_list2, CustomList([1, 1, 1, 1])):
+            self.assertEqual(i, j)
 
-        # Diff size
+    def test_add_diff_size(self):
+        "Test add diff sizes CustomLists"
+
+        cust_list1 = CustomList([5, 1, 3, 7])
         cust_list2 = CustomList([1, 1])
-        self.assertEqual(self.cust_list1 + cust_list2, CustomList([6, 2, 3, 7]))
+        res_list1 = cust_list1 + cust_list2
+        res_list2 = cust_list2 + cust_list1
+        exp_list = CustomList([6, 2, 3, 7])
+        # Check resulted list
+        self.assertEqual(len(res_list1), len(exp_list))
+        for i, j in zip(res_list1, exp_list):
+            self.assertEqual(i, j)
+        self.assertEqual(len(res_list2), len(exp_list))
+        for i, j in zip(res_list2, exp_list):
+            self.assertEqual(i, j)
+        # Check cust_list1 and cust_list2 didnt changed
+        for i, j in zip(cust_list1, CustomList([5, 1, 3, 7])):
+            self.assertEqual(i, j)
+        for i, j in zip(cust_list2, CustomList([1, 1])):
+            self.assertEqual(i, j)
 
-        # With list
+    def test_add_with_list_same_size(self):
+        "Test add CustomList and list with same size"
+
+        cust_list1 = CustomList([5, 1, 3, 7])
+        lst = [1, 1, 1, 1]
+        res_list = cust_list1 + lst
+        exp_list = CustomList([6, 2, 4, 8])
+        # Check resulted list
+        self.assertEqual(len(res_list), len(exp_list))
+        for i, j in zip(res_list, exp_list):
+            self.assertEqual(i, j)
+        # Check cust_list1 and cust_list2 didnt changed
+        for i, j in zip(cust_list1, CustomList([5, 1, 3, 7])):
+            self.assertEqual(i, j)
+        for i, j in zip(lst, [1, 1, 1, 1]):
+            self.assertEqual(i, j)
+
+    def test_add_with_list_diff_size(self):
+        "Test add CustomList and list with diff sizes"
+
+        cust_list1 = CustomList([5, 1, 3, 7])
         lst = [1, 1]
-        self.assertEqual(self.cust_list1 + lst, CustomList([6, 2, 3, 7]))
-        self.assertEqual(lst + self.cust_list1, CustomList([6, 2, 3, 7]))
+        res_list1 = cust_list1 + lst
+        res_list2 = lst + cust_list1
+        exp_list = CustomList([6, 2, 3, 7])
+        # Check resulted list
+        self.assertEqual(len(res_list1), len(exp_list))
+        for i, j in zip(res_list1, exp_list):
+            self.assertEqual(i, j)
+        self.assertEqual(len(res_list2), len(exp_list))
+        for i, j in zip(res_list2, exp_list):
+            self.assertEqual(i, j)
+        # Check cust_list1 and cust_list2 didnt changed
+        for i, j in zip(cust_list1, CustomList([5, 1, 3, 7])):
+            self.assertEqual(i, j)
+        for i, j in zip(lst, [1, 1]):
+            self.assertEqual(i, j)
 
-    def test_sub(self):
-        "Test sub"
+    def test_sub_same_size(self):
+        "Test sub same size CustomLists"
 
-        # Same size
+        cust_list1 = CustomList([5, 1, 3, 7])
         cust_list2 = CustomList([1, 1, 1, 1])
-        self.assertEqual(self.cust_list1 - cust_list2, CustomList([4, 0, 2, 6]))
+        res_list1 = cust_list1 - cust_list2
+        res_list2 = cust_list2 - cust_list1
+        exp_list1 = CustomList([4, 0, 2, 6])
+        exp_list2 = CustomList([-4, 0, -2, -6])
+        # Check resulted list
+        self.assertEqual(len(res_list1), len(exp_list1))
+        for i, j in zip(res_list1, exp_list1):
+            self.assertEqual(i, j)
+        self.assertEqual(len(res_list2), len(exp_list2))
+        for i, j in zip(res_list2, exp_list2):
+            self.assertEqual(i, j)
+        # Check cust_list1 and cust_list2 didnt changed
+        for i, j in zip(cust_list1, CustomList([5, 1, 3, 7])):
+            self.assertEqual(i, j)
+        for i, j in zip(cust_list2, CustomList([1, 1, 1, 1])):
+            self.assertEqual(i, j)
 
-        # Diff size
+    def test_sub_diff_size(self):
+        "Test sub diff sizes CustomLists"
+
+        cust_list1 = CustomList([5, 1, 3, 7])
         cust_list2 = CustomList([1, 1])
-        self.assertEqual(self.cust_list1 - cust_list2, CustomList([4, 0, 3, 7]))
+        res_list1 = cust_list1 - cust_list2
+        res_list2 = cust_list2 - cust_list1
+        exp_list1 = CustomList([4, 0, 3, 7])
+        exp_list2 = CustomList([-4, 0, -3, -7])
+        # Check resulted list
+        self.assertEqual(len(res_list1), len(exp_list1))
+        for i, j in zip(res_list1, exp_list1):
+            self.assertEqual(i, j)
+        self.assertEqual(len(res_list2), len(exp_list2))
+        for i, j in zip(res_list2, exp_list2):
+            self.assertEqual(i, j)
+        # Check cust_list1 and cust_list2 didnt changed
+        for i, j in zip(cust_list1, CustomList([5, 1, 3, 7])):
+            self.assertEqual(i, j)
+        for i, j in zip(cust_list2, CustomList([1, 1])):
+            self.assertEqual(i, j)
 
-        # With list
+    def test_sub_with_list_same_size(self):
+        "Test sub CustomList and list with same size"
+
+        cust_list1 = CustomList([5, 1, 3, 7])
+        lst = [1, 1, 1, 1]
+        res_list1 = cust_list1 - lst
+        res_list2 = lst - cust_list1
+        exp_list1 = CustomList([4, 0, 2, 6])
+        exp_list2 = CustomList([-4, 0, -2, -6])
+        # Check resulted list
+        self.assertEqual(len(res_list1), len(exp_list1))
+        for i, j in zip(res_list1, exp_list1):
+            self.assertEqual(i, j)
+        self.assertEqual(len(res_list2), len(exp_list2))
+        for i, j in zip(res_list2, exp_list2):
+            self.assertEqual(i, j)
+        # Check cust_list1 and cust_list2 didnt changed
+        for i, j in zip(cust_list1, CustomList([5, 1, 3, 7])):
+            self.assertEqual(i, j)
+        for i, j in zip(lst, [1, 1, 1, 1]):
+            self.assertEqual(i, j)
+
+    def test_sub_with_list_diff_size(self):
+        "Test sub CustomList and list with diff sizes"
+
+        cust_list1 = CustomList([5, 1, 3, 7])
         lst = [1, 1]
-        self.assertEqual(self.cust_list1 - lst, CustomList([4, 0, 3, 7]))
-        self.assertEqual(lst - self.cust_list1, CustomList([-4, 0, -3, -7]))
+        res_list1 = cust_list1 - lst
+        res_list2 = lst - cust_list1
+        exp_list1 = CustomList([4, 0, 3, 7])
+        exp_list2 = CustomList([-4, 0, -3, -7])
+        # Check resulted list
+        self.assertEqual(len(res_list1), len(exp_list1))
+        for i, j in zip(res_list1, exp_list1):
+            self.assertEqual(i, j)
+        self.assertEqual(len(res_list2), len(exp_list2))
+        for i, j in zip(res_list2, exp_list2):
+            self.assertEqual(i, j)
+        # Check cust_list1 and cust_list2 didnt changed
+        for i, j in zip(cust_list1, CustomList([5, 1, 3, 7])):
+            self.assertEqual(i, j)
+        for i, j in zip(lst, [1, 1]):
+            self.assertEqual(i, j)
 
     def test_compare(self):
         "Test comparison"
 
+        cust_list1 = CustomList([5, 1, 3, 7])
         cust_list2 = CustomList([7, 3, 1, 5])
-        self.assertTrue(self.cust_list1 == cust_list2)
-        self.assertTrue(self.cust_list1 >= cust_list2)
-        self.assertTrue(self.cust_list1 <= cust_list2)
-        self.assertFalse(self.cust_list1 > cust_list2)
+        self.assertTrue(cust_list1 == cust_list2)
+        self.assertTrue(cust_list1 >= cust_list2)
+        self.assertTrue(cust_list1 <= cust_list2)
+        self.assertFalse(cust_list1 > cust_list2)
 
         cust_list2.append(1)
-        self.assertTrue(self.cust_list1 < cust_list2)
-        self.assertTrue(self.cust_list1 <= cust_list2)
-        self.assertTrue(self.cust_list1 != cust_list2)
-        self.assertFalse(self.cust_list1 > cust_list2)
-        self.assertFalse(self.cust_list1 == cust_list2)
+        self.assertTrue(cust_list1 < cust_list2)
+        self.assertTrue(cust_list1 <= cust_list2)
+        self.assertTrue(cust_list1 != cust_list2)
+        self.assertFalse(cust_list1 > cust_list2)
+        self.assertFalse(cust_list1 == cust_list2)
 
     def test_str(self):
         "Test str"
 
-        self.assertEqual(str(self.cust_list1), "[5, 1, 3, 7]  Sum: 16")
+        self.assertEqual(str(CustomList([5, 1, 3, 7])), "[5, 1, 3, 7]  Sum: 16")
 
     def test_repr(self):
         "Test repr"
 
-        self.assertEqual(repr(self.cust_list1), "CustomList([5, 1, 3, 7])")
-
-    def test_non_numeric_types(self):
-        "Test create CustomList with non numeric list"
-
-        with self.assertRaises(TypeError):
-            CustomList(["E", "n", "d"])
-
-        with self.assertRaises(TypeError):
-            self.cust_list1.append([1, 2, 3])
-
-        with self.assertRaises(TypeError):
-            self.cust_list1.extend([1, "2", 3])
-
-        with self.assertRaises(TypeError):
-            self.cust_list1.insert(2, "2")
+        self.assertEqual(repr(CustomList([5, 1, 3, 7])), "CustomList([5, 1, 3, 7])")
 
     def test_empty_list(self):
         "Test creating CustomList with empty list"
@@ -116,9 +249,10 @@ class TestDecor(TestCase):
     def test_float(self):
         "Test CustomList with floats"
 
+        cust_list1 = CustomList([5, 1, 3, 7])
         cust_list2 = CustomList([5, 1, 3, 7.0])
-        self.assertTrue(self.cust_list1 == cust_list2)
+        self.assertTrue(cust_list1 == cust_list2)
         cust_list2[-1] += 0.1
-        self.assertFalse(self.cust_list1 == cust_list2)
+        self.assertFalse(cust_list1 == cust_list2)
         cust_list2[-1] -= 0.100000001
-        self.assertTrue(self.cust_list1 == cust_list2)
+        self.assertTrue(cust_list1 == cust_list2)
