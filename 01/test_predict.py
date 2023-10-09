5b475b151a6f1afdfb54fab93f8e4ace8ecba9a7
Dadypool <dudypool@yandex.ru>
2023-10-09 10:08:55 +0300

    Added tests for predict

---
+++ 01/test_predict.py
@@ -0,0 +1,50 @@
+"Homework 1 with tests for predict.py"
+
+from unittest import TestCase, mock
+
+from predict import predict_message_mood
+
+
+class TestPredict(TestCase):
+    "Class with tests"
+
+    def setUp(self):
+        self.model = mock.Mock()
+        self.model.predict.return_value = 0.5
+
+    def test_base_work(self):
+        "Test base work"
+
+        self.assertEqual("норм", predict_message_mood("База", self.model))
+        self.model.predict.return_value = 0.1
+        self.assertEqual("неуд", predict_message_mood("Не норм", self.model))
+        self.model.predict.return_value = 1.0
+        self.assertEqual("отл", predict_message_mood("Пере норм", self.model))
+
+    def test_edge_cases(self):
+        "Tests edge cases"
+
+        self.model.predict.return_value = 0.3
+        self.assertEqual("норм", predict_message_mood("Около левый край", self.model))
+        self.model.predict.return_value = 0.8
+        self.assertEqual("норм", predict_message_mood("Около правый край", self.model))
+
+        self.model.predict.return_value = 0.2
+        self.assertEqual("неуд", predict_message_mood("Левый край", self.model))
+        self.model.predict.return_value = 0.9
+        self.assertEqual("отл", predict_message_mood("Правый край", self.model))
+
+    def test_change_thresholds(self):
+        "Test chainging tresholds"
+
+        self.assertEqual("неуд", predict_message_mood("База", self.model, 0.6))
+        self.model.predict.return_value = 0.9
+        self.assertEqual("норм", predict_message_mood("Не норм", self.model, 0.3, 0.9))
+        self.model.predict.return_value = 0.4
+        self.assertEqual("отл", predict_message_mood("Пере норм", self.model, 0.1, 0.3))
+
+    def test_model_called_once(self):
+        "Test model predict func called once"
+
+        predict_message_mood("База", self.model)
+        self.model.predict.assert_called_once()
