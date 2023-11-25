7ef8f4d1672e6f328099b9e123beb7ef9803c64d
Dadypool <dudypool@yandex.ru>
2023-11-25 14:44:52 +0300

    Fix import module

--- 11/setup.py
+++ 11/setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 
 def main():
     setup(name="cjson",
           version="1.0.0",
-          description="Python module for json parsing and serialization",
+          description="Python module in C for json parsing and serialization",
           ext_modules=[Extension("cjson", ["cjson.c"])])
 
 
